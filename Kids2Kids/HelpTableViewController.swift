6538bc3aa191b41be8530e6ac2887464e0988f1f
Oleksii Liubarets <lubaretsaa@gmail.com>
2017-08-10 16:05:28 +0300

    add info to help page

--- Kids2Kids/HelpTableViewController.swift
+++ Kids2Kids/HelpTableViewController.swift
@@ -7,14 +7,17 @@
 //
 
 import UIKit
+import MessageUI
 
 class HelpTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        checkAvailableMailServices()
         setup()
     }
-
+    
     func setup() {
         
         self.navigationItem.title = "Помощь"
@@ -29,4 +32,66 @@ class HelpTableViewController: UITableViewController {
         self.tableView.backgroundView = imageView
     }
     
+    private func callNumber(phoneNumber: String) {
+        
+        if let phoneCallURL = URL(string: "tel://\(phoneNumber)") {
+            
+            let application:UIApplication = UIApplication.shared
+            if (application.canOpenURL(phoneCallURL)) {
+                application.open(phoneCallURL, options: [:], completionHandler: nil)
+            }
+        }
+    }
+    
+    private func showAlertController(title: String, message: String, accessTitle: String, completion: @escaping ()->()) {
+        let showAlert : UIAlertController = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        showAlert.addAction(UIAlertAction(title: "Отмена", style: UIAlertActionStyle.cancel, handler: nil))
+        showAlert.addAction(UIAlertAction(title: accessTitle, style: UIAlertActionStyle.default, handler: { action in completion()}))
+        self.present(showAlert, animated: true, completion: nil)
+    }
+
+    
+    @IBAction func phoneCall(_ sender: UIButton) {
+        showAlertController(title: "Позвонить в Фонд ДетиДетям?", message: "+38 (050) 471-30-30", accessTitle: "Позвонить") {
+            self.callNumber(phoneNumber: "+380504713030")
+        }
+    }
+    
+    @IBAction func email(_ sender: UIButton) {
+        sendEmail()
+    }
+    
+    
 }
+
+extension HelpTableViewController: MFMailComposeViewControllerDelegate {
+    
+    // MARK: Mail Services
+    
+    func checkAvailableMailServices() {
+        if !MFMailComposeViewController.canSendMail() {
+            print("Mail services are not available")
+            return
+        }
+    }
+    
+    func sendEmail() {
+        let composeVC = MFMailComposeViewController()
+        composeVC.mailComposeDelegate = self
+        // Configure the fields of the interface.
+        composeVC.setToRecipients(["web@kids2kids-fund.com"])
+        //        composeVC.setSubject("")
+        //        composeVC.setMessageBody("", isHTML: false)
+        // Present the view controller modally.
+        self.present(composeVC, animated: true, completion: nil)
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController,
+                               didFinishWith result: MFMailComposeResult,
+                               error: Error?) {
+        // Check the result or perform other tasks.
+        // Dismiss the mail compose view controller.
+        controller.dismiss(animated: true, completion: nil)
+    }
+}
+
